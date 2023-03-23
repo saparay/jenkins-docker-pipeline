ee99c4f52aea9c6fd8ce98a250dff882644916fe
Saparay Mani Chandra <saparay4@gmail.com>
2023-03-23 07:56:33 +0000

    first commit

---
+++ src/main/java/com/jenkins/docker/pipeline/ComJenkinsDockerPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.docker.pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComJenkinsDockerPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComJenkinsDockerPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jenkins/docker/pipeline/ComJenkinsDockerPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.docker.pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComJenkinsDockerPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

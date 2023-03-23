27e3499a8aa47b749ff912258b0423ed06120b8f
Saparay Mani Chandra <saparay4@gmail.com>
2023-03-23 08:16:19 +0000

    Second Commit

---
+++ src/main/java/com/jenkins/docker/pipeline/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.jenkins.docker.pipeline.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1")
+public class HomeController {
+
+	@GetMapping(path = "/home")
+	public String homePage() {
+		return "Home Page";
+	}
+}
